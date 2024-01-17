6a03d7a2a9866a05a7279bf42317345dc483cebb
LilHbt <lilianhumblot@gmail.com>
2024-01-17 17:04:33 +0100

    first commit

---
+++ src/containers/Form/index.test.js
@@ -0,0 +1,31 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import Form from "./index";
+
+describe("When Form is created", () => {
+  it("a list of fields is displayed", async () => {
+    render(<Form />);
+    await screen.findByText("Email");
+    await screen.findByText("Nom");
+    await screen.findByText("Prénom");
+    await screen.findByText("Personel / Entreprise");
+    await screen.findByText("Envoyer");
+  });
+
+  describe("and a click is triggered on the submit button", () => {
+    it("the success action is called", async () => {
+      const onSuccess = jest.fn();
+
+      render(<Form onSuccess={onSuccess} />);
+      fireEvent(
+        await screen.findByTestId("button-test-id"),
+        new MouseEvent("click", {
+          cancelable: true,
+          bubbles: true,
+        })
+      );
+      await screen.findByText("En cours");
+      console.log("onSuccess calls:", onSuccess.mock.calls);
+      expect(onSuccess).toHaveBeenCalled();
+    });
+  });
+});
